477441b6bb6eeda6ec8355cc0aff98c733ab5288
dlmnawodani <malshanawodani0916@gmail.com>
2024-05-05 22:23:10 +0530

    Login , SignUp UIs

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 export const routes: Routes = [
     {
@@ -11,5 +12,9 @@ export const routes: Routes = [
         path: 'login',
         component: LoginComponent
     },
-    
+    {
+        path: 'signup',
+        component: SignupComponent
+    },
+
 ];
